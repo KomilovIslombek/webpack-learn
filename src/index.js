ed7fee54433819f47bc1561b82bb428e928ccc78
KomilovIslombek <islambek445@gmail.com>
2024-03-02 10:53:47 +0500

    add html-webpack-plugin for work with files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import styles from './styles/index.scss'
+
 import { getTitle } from './config/seo'
 
 const title = getTitle()
 document.title = title
 console.log(title)
+console.log('styles', styles)
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,56 @@
 const path = require('path') // node js
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   // mode
   mode: 'development', // production
   // entry
-  entry: path.resolve(__dirname, 'src/index.js'),
+  entry: {
+    index: path.resolve(__dirname, 'src/index.js'),
+    about: path.resolve(__dirname, './src/about.js'),
+  },
   // output
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
   // plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      // title
+      title: 'Home',
+      // filename
+      filename: 'index.html',
+      // template
+      template: './src/temp.index.html',
+      // chunks
+      chunks: ['index'],
+    }),
+    new HtmlWebpackPlugin({
+      // title
+      title: 'About',
+      // filename
+      filename: 'about.html',
+      // template
+      template: './src/temp.about.html',
+      // chunks
+      chunks: ['about'],
+    }),
+  ],
   // loaders
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 }
